08eeac84645da38ec64281f73d115230d701e7a4
Erik Rose <erik@mozilla.com>
2011-02-10 14:46:24 -0800

    Add another context manager for moving the cursor down to the progress bar and then bringing it back. Put actual content in the progress bar, and invert the text.

--- noseprogressive/plugin.py
+++ noseprogressive/plugin.py
@@ -36,7 +36,6 @@ class ProgressivePlugin(Plugin):
 
         self.stream = stream
         setupterm()  # TODO: Don't do this if self.stream isn't a terminal. Use os.isatty(self.stream.fileno()). If it isn't, perhaps replace the ShyProgressBar with a dummy object.
-        self.shy_progress_bar = ShyProgressBar(self.stream)
         return DevNullStream()
 
     def getDescription(self, test):
@@ -44,7 +43,7 @@ class ProgressivePlugin(Plugin):
 
     def printError(self, kind, err, test):
         formatted_err = format_exception(*err)
-        with self.shy_progress_bar:
+        with ShyProgressBar(self.stream, test):
             writeln = self.stream.writeln
             writeln()
             writeln('=' * 70)
@@ -54,14 +53,14 @@ class ProgressivePlugin(Plugin):
 
     def printErrors(self):
         # The current summary doesn't begin with a \n.
-        with self.shy_progress_bar:
+        with ShyProgressBar(self.stream, test):
             self.stream.writeln()
 
     # TODO: Override printSummary() to add footer.
 
     def addError(self, test, err):
         exc, val, tb = err
-        with self.shy_progress_bar:
+        with ShyProgressBar(self.stream, test):
             if isinstance(exc, SkipTest):
                 self.stream.writeln()
                 self.stream.writeln('SKIP: %s' % nice_test_address(test))
@@ -73,45 +72,54 @@ class ProgressivePlugin(Plugin):
 
     def addSkip(self, test, reason):
         # Only in 2.7+
-        with self.shy_progress_bar:
+        with ShyProgressBar(self.stream, test):
             self.stream.writeln()
             self.stream.writeln('SKIP: %s' % nice_test_address(test))
 
     def addSuccess(self, test):
-        with self.shy_progress_bar:
-            self.stream.writeln(get_context(test.test) + '.' + test.test._testMethodName)
+        with AtProgressBar(self.stream):
+            self.stream.write(progress_bar(test))
 
 
-class ShyProgressBar(_WritelnDecorator):
-    """A progress bar that gets out of the way, lets you print what you want, and then springs back into place"""
+def progress_bar(test):
+    REV = tigetstr('rev')
+    return tigetstr('el') + REV + get_context(test.test) + '.' + test.test._testMethodName + REV
 
+
+class AtProgressBar(object):
     def __init__(self, stream):
         self.stream = stream
 
-    def move_to_progress_bar(self):
+    def __enter__(self):
         """Save position and move to progress bar, col 1."""
         self.stream.write(tigetstr('sc'))  # save position
         width, height = tigetnum('cols'), tigetnum('lines')
         self.stream.write(tparm(tigetstr('cup'), height, 0))
     
-    def move_back(self):
-        self.stream.write(tigetstr('rc'))  # restore position        
-    
+    def __exit__(self, type, value, tb):
+        self.stream.write(tigetstr('rc'))  # restore position
+
+
+class ShyProgressBar(object):
+    """Context manager that implements a progress bar that gets out of the way
+    """
+
+    def __init__(self, stream, test):
+        self.stream = stream
+        self.test = test
+
     def __enter__(self):
+        """Erase the progress bar so bits of disembodied progress bar don't get scrolled up the terminal."""
         # My terminal has no status line, so we make one manually.
         # Doing this each time gives us a hope of adjusting if people resize their terminals during test runs:
-        self.move_to_progress_bar()
-        self.stream.write(tigetstr('el'))  # erase to EOL
-        self.move_back()
-        
+        with AtProgressBar(self.stream):
+            self.stream.write(tigetstr('el'))  # erase to EOL
         self.stream.flush()
 
     def __exit__(self, type, value, tb):
-        # TODO: Redraw progress bar.
-        self.move_to_progress_bar()
-        self.stream.write(''.join(random.choice('ABCDEF') for x in xrange(30)))  # print progress bar
-        self.move_back()
-
+        """Redraw the progress bar at the bottom of the terminal."""
+        with AtProgressBar(self.stream):
+            self.stream.write(progress_bar(self.test))
         self.stream.flush()
 
 
